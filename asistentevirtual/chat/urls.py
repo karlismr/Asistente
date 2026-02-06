0ff9a3ae6fe72426843892f27a72c5c5b41bfd88
karlismr <katittaa24@gmail.com>
2026-02-05 21:06:25 -0300

    admin

--- asistentevirtual/chat/urls.py
+++ asistentevirtual/chat/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from .views import chat_view, configurar_asistente
+from .views import chat_view, configurar_asistente, admin 
 from django.contrib.auth import views as auth_views
 from . import views 
 
 urlpatterns = [
     path('', chat_view, name='chat_view'),
+    path('admin/', admin.site.urls),
       path('configurar/', configurar_asistente, name='configurar_asistente'),
       path('registro/', views.registro, name='registro'),
     path('login/', auth_views.LoginView.as_view(template_name='chat/login.html'), name='login'),
