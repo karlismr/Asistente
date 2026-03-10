f9325621567fb4f99dcf73f3827948bd70d60f4d
karlismr <katittaa24@gmail.com>
2026-03-10 19:52:27 -0300

    Cambio de orden

--- asistentevirtual/mysite/asgi.py
+++ asistentevirtual/mysite/asgi.py
@@ -1,10 +1,17 @@
 import os
+import django
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
-import chat.routing 
+
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mysite.settings')
+
+django.setup()
+
+django_asgi_app = get_asgi_application()
+
+import chat.routing
 
 application = ProtocolTypeRouter({
     # Tráfico normal
